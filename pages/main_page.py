c6463e953b4f65753a2195fe4c6f5595d7eab440
Chyna <chynawright06@gmail.com>
2024-06-04 16:19:53 -0500

    AQA-183

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.common.by import By
-
+from time import sleep
 from pages.base_page import Page
 
 
 class MainPage(Page):
     OFF_PLAN = (By.CSS_SELECTOR, '[class*="menu-twobutton"]')
+    OFF_PLAN_BOTTOM = By.CSS_SELECTOR, '[class*="menu-link w-inline-block w--current"]'
     def click_off_plan(self):
-        self.find_element(*self.OFF_PLAN).click()
+        self.wait_until_clickable_click(*self.OFF_PLAN_BOTTOM)
+        # self.find_element(*self.OFF_PLAN).click()
--- pages/total_projects_page.py
+++ pages/total_projects_page.py
@@ -1,4 +1,6 @@
+from selenium.webdriver.common import by
 from selenium.webdriver.common.by import By
+from time import sleep
 
 from pages.base_page import Page
 
@@ -6,10 +8,14 @@ from pages.base_page import Page
 class TotalProjectsPage(Page):
 
     PROJECT = (By.CSS_SELECTOR, "[wized*='cardOfProperty']")
-    ARCH = (By.CSS_SELECTOR, '[data-w-tab*="Tab 1"]')
+    PROJECT_TWO = (By.CSS_SELECTOR, "[w-el-class*='project-card w-inline-block']")
+    MENU = (By.CSS_SELECTOR, "[class='menu-mobile hero-menu']")
+    OFF_PLAN_BOTTOM = (By.CSS_SELECTOR, '[class*="menu-link w-inline-block w--current"]')
+
 
     def click_first_product(self):
         self.wait_until_clickable_click(*self.PROJECT)
+
 
 
 
