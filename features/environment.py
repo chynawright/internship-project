d2bdc251d3e56c06bc43606db08bcefae258fb87
Chyna <chynawright06@gmail.com>
2024-06-06 12:44:39 -0500

    AQA-215

--- features/environment.py
+++ features/environment.py
@@ -2,15 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.wait import WebDriverWait
 
 from app.application import Application
 
 
-def browser_init(context):
-    """
-    :param context: Behave context
-    """
+def browser_init(context, scenario_name):
+    # """
+    # :param context: Behave context
+    # """
     # driver_path = ChromeDriverManager().install()
     # service = Service(driver_path)
     # context.driver = webdriver.Chrome(service=service)
@@ -19,15 +20,29 @@ def browser_init(context):
     # service = Service(driver_path)
     # context.driver = webdriver.Firefox(service=service)
 
-
     ### HEADLESS MODE ####
-    options = webdriver.ChromeOptions()
-    options.add_argument('headless')
-    service = Service(ChromeDriverManager().install())
-    context.driver = webdriver.Chrome(
-         options=options,
-         service=service
-     )
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('headless')
+    # service = Service(ChromeDriverManager().install())
+    # context.driver = webdriver.Chrome(
+    # options=options,
+    # service=service
+    # )
+
+    bs_user = 'chyna_1iNWTw'
+    bs_key = 'foTxpt4R8yYQnqzeczmT'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+
+    options = Options()
+    bstack_options = {
+        'os': 'Windows',
+        'osVersion': '10',
+        'browserName': 'Chrome',
+        'sessionName': scenario_name
+
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
 
     context.driver.maximize_window()
 
@@ -40,7 +55,7 @@ def browser_init(context):
 
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    browser_init(context, scenario_name='AQA-215')
 
 
 def before_step(context, step):
--- pages/main_page.py
+++ pages/main_page.py
@@ -7,5 +7,5 @@ class MainPage(Page):
     OFF_PLAN = (By.CSS_SELECTOR, '[class*="menu-twobutton"]')
     OFF_PLAN_BOTTOM = By.CSS_SELECTOR, '[class*="menu-link w-inline-block w--current"]'
     def click_off_plan(self):
-        self.wait_until_clickable_click(*self.OFF_PLAN_BOTTOM)
-        # self.find_element(*self.OFF_PLAN).click()
+        # self.wait_until_clickable_click(*self.OFF_PLAN_BOTTOM)
+        self.find_element(*self.OFF_PLAN).click()
